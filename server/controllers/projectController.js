ffda7523ffbb2e418e2ad2e1828585bfd1b8e4ec
MichelLosier <mikelosier@gmail.com>
2017-05-20 15:07:50 -0700

    server project model populates references to artwork models

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -2,22 +2,28 @@ var Project = require('../models/project');
 
 //get all projects
 exports.listAll = function(req, res, next){
-	Project.find({}, function(err, docs) {
+	Project.find()
+	.populate('featuredImage gallery')
+	.exec(function(err, doc){
 		if(err) return console.error(err);
-		res.json(docs);
+		res.json(doc);
 	});
 };
 //get all projects by category type
 exports.categoryListAll = function (req, res, next) {
-	Project.find({ category: req.params.cat }, function(err, docs) {
+	Project.find({ category: req.params.cat })
+	.populate('featuredImage gallery')
+	.exec(function(err, doc){
 		if(err) return console.error(err);
-		res.json(docs);
+		res.json(doc);
 	});
 };
 
 //get project by id
 exports.queryID = function(req, res, next) {
-	Project.findOne({_id: req.params._id}, function(err, doc){
+	Project.findOne({_id: req.params._id})
+	.populate('featuredImage gallery')
+	.exec(function(err, doc){
 		if(err) return console.error(err);
 		res.json(doc);
 	});
--- server/models/project.js
+++ server/models/project.js
@@ -1,15 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Artwork = require ('./artwork');
 
 var projectSchema = new Schema({
 	name: String,
 	position: Number,
 	dateAdded: { type: Date, default: Date.now },
 	description: String,
-	featuredImage: String,
+	featuredImage: {type: Schema.Types.ObjectId, ref: 'artwork'},
 	category: String,
 	tags: [String],
-	gallery: [String]
+	gallery: [{type: Schema.Types.ObjectId, ref: 'artwork'}]
 });
 
 var Project = mongoose.model('project', projectSchema);
